ccdedce624b203dbef30c20936ce8a5dba1bcfcb
go-nishiumi <giannisann34@gmail.com>
2024-11-26 17:54:04 +0900

    fix: github actionsでのエラーを修正

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-    webServer: {
-        command: 'npm start',
-        port: 3000
-    },
     projects: [
         {
             name: 'Google Chrome',
